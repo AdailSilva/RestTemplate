b3518024fecc3f9c5cdf2cf659517ff3ba7cdf2b
Adail Silva <adail@bdmg.mg.gov.br>
2021-11-15 10:22:55 -0300

    Initial commit

---
+++ src/test/java/com/adailsilva/resttemplate/RestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adailsilva.resttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
